04f12be6d59fc61ecf8e07b30f10646e399c74f9
rdx420 <padraramesh420@gmail.com>
2022-06-30 08:30:20 +0530

    violet: prepare vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/rdx420/vendor_xiaomi_violet.git -b eleven vendor/xiaomi/violet
+git clone https://github.com/karthik558/MsM-4.14-K2-141b.git -b eleven kernel/xiaomi/violet
+git clone https://gitlab.com/dotos-devices/vendor_ANXCamera.git -b dot11 vendor/ANXCamera
